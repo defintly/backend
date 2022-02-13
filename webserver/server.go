6965d825a25eb6ba6a5f38ab271e454067283284
Steven Knoblich <steven.knoblich@gmail.com>
2022-02-13 19:17:51 +0100

    add routes to server start-up

--- webserver/server.go
+++ webserver/server.go
@@ -3,11 +3,13 @@ package webserver
 import (
 	"fmt"
 	"github.com/defintly/backend/general"
+	"github.com/defintly/backend/webserver/authentication"
 	"github.com/defintly/backend/webserver/categories"
 	"github.com/defintly/backend/webserver/collections"
 	"github.com/defintly/backend/webserver/concepts"
 	"github.com/defintly/backend/webserver/criteria"
 	"github.com/defintly/backend/webserver/handler"
+	"github.com/defintly/backend/webserver/users"
 	"github.com/gin-gonic/gin"
 	"github.com/toorop/gin-logrus"
 	"net/http"
@@ -18,13 +20,17 @@ func Run(hostname string, port int) {
 	router.Use(ginlogrus.Logger(general.Log), gin.Recovery())
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"name": "defintly API", "version": "2.0.1"})
+		ctx.JSON(http.StatusOK, gin.H{"name": "defintly API", "version": "2.1.0"})
 	})
+
+	authHandler := handler.AuthenticationHandler()
 
 	initCategoryRoutes(router)
 	initCollectionRoutes(router)
-	initConceptRoutes(router)
+	initConceptRoutes(router, authHandler)
 	initCriteriaRoutes(router)
+	initAuthRoutes(router, authHandler)
+	initUserRoutes(router, authHandler)
 
 	panic(router.Run(fmt.Sprintf("%s:%d", hostname, port)))
 }
@@ -47,13 +53,23 @@ func initCollectionRoutes(router *gin.Engine) {
 	collectionIdGroup.GET("", collections.GetById())
 }
 
-func initConceptRoutes(router *gin.Engine) {
+func initConceptRoutes(router *gin.Engine, authHandler gin.HandlerFunc) {
 	conceptGroup := router.Group("/concepts")
 
 	conceptGroup.GET("", concepts.GetAll())
 
 	conceptIdGroup := conceptGroup.Group("/:id", handler.Id())
 	conceptIdGroup.GET("", concepts.GetById())
+	conceptIdGroup.GET("/comments", concepts.GetComments())
+	conceptIdGroup.POST("/comments", authHandler, concepts.AddComment())
+
+	conceptCommentGroup := conceptGroup.Group("/comments")
+	conceptCommentGroup.GET("/list-unreviewed", authHandler, concepts.ListUnreviewedComments())
+
+	conceptCommentIdGroup := conceptCommentGroup.Group("/:id", handler.Id())
+	conceptCommentIdGroup.DELETE("/", concepts.DeleteComment())
+	conceptCommentIdGroup.GET("/children", concepts.GetCommentList())
+	conceptCommentIdGroup.PUT("/allow", concepts.AllowComment())
 }
 
 func initCriteriaRoutes(router *gin.Engine) {
@@ -63,4 +79,24 @@ func initCriteriaRoutes(router *gin.Engine) {
 
 	criteriaIdGroup := criteriaGroup.Group("/:id", handler.Id())
 	criteriaIdGroup.GET("", criteria.GetById())
+}
+
+func initAuthRoutes(router *gin.Engine, authHandler gin.HandlerFunc) {
+	authGroup := router.Group("/auth")
+
+	authGroup.POST("/login", authentication.Login())
+	authGroup.POST("/register", authentication.Register())
+	authGroup.PUT("/mail", authHandler, authentication.ChangeMailAddress())
+	authGroup.PUT("/password", authHandler, authentication.ChangePassword())
+}
+
+func initUserRoutes(router *gin.Engine, authHandler gin.HandlerFunc) {
+	usersGroup := router.Group("/users", authHandler)
+
+	usersIdGroup := usersGroup.Group("/:userId", handler.UserIdWithMeHandler())
+
+	usersIdGroup.GET("", users.Get())
+	usersIdGroup.PUT("/username", users.ChangeUsername())
+	usersIdGroup.PUT("/firstname", users.ChangeFirstname())
+	usersIdGroup.PUT("/lastname", users.ChangeLastname())
 }
